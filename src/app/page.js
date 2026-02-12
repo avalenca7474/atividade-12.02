cd1617eefa4d8e3f65bacd40df25804e7121ae99
avalenca7474 <adrianofacul.unicap74@gmail.com>
2026-02-12 18:35:40 +0000

    Corrigindo estrutura e adicionando mensagem

---
+++ src/app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', fontSize: '2rem' }}>
+      <h1>Atividade do dia 12/02.</h1>
+    </div>
+  );
+}
